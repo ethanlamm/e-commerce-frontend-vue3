bfbe203a6aaf3452b390ad3dc61c9cbf64952613
Ethan <1028716801@qq.com>
2022-07-22 17:04:00 +0800

    关闭eslint多行检查

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-multiple-empty-lines': [0, { max: 100 }]
   }
 }
