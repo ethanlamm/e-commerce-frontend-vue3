cad0e46a636c11743e5abc6f9dd29e09c8ff1da7
Ethan <1028716801@qq.com>
2022-07-31 10:59:59 +0800

    退出登录

--- src/components/app-topnav.vue
+++ src/components/app-topnav.vue
@@ -9,7 +9,7 @@
               {{ profile.account }}
             </a>
           </li>
-          <li><a href="javascript:;">退出登录</a></li>
+          <li><a href="javascript:;" @click="logout">退出登录</a></li>
         </template>
         <template v-else>
           <li><router-link to="/login">请先登录</router-link></li>
@@ -29,15 +29,26 @@
 <script>
 import { computed } from 'vue'
 import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
 export default {
   name: 'AppTopnav',
   setup (props) {
     const store = useStore()
+    const router = useRouter()
     //  用户信息，计算属性--响应式
     const profile = computed(() => {
       return store.state.user.profile
     })
-    return { profile }
+
+    // 退出登录
+    // 1.清空本地存储和vuex存储的用户信息
+    // 2.跳转至登录页
+    const logout = () => {
+      // 本地存储是拿vuex数据存储的，当重置为空对象{}时，本地存储也清空了数据(router/index.js)
+      store.commit('user/setUser', {})
+      router.push('/login')
+    }
+    return { profile, logout }
   }
 }
 </script>
